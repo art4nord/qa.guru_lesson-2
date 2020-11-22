db3ec327ac28d2d330de9518775cc2780dfb5c54
stanislavd <stanislav.dmitruk@neugelb.com>
2020-11-22 01:15:19 +0100

    Updated fill-form methods. Added data checks.

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -8,12 +8,11 @@ import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationFormTest {
 
-//  startMaximized is not necessary for my objectives so it's turned off for now
+//  "startMaximized" is not necessary for my objectives so it's turned off for now.
 /*  @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
@@ -24,12 +23,23 @@ public class StudentRegistrationFormTest {
         String varFirstName ="Stanislav",
                 varLastName = "Dmitruk",
                 varUserEmail = "stanislavtest@gmail.com",
+                varGender = "Other",
                 varUserNumber = "1234567890",
-                hobbiesCheckbox = "",
-                uploadPicture = "",
+                varBirthDay = "20",
+                varBirthMonth = "February",
+                varBirthYear = "1991",
+                varSubject1Prefix = "e",
+                varSubject1Full = "English",
+                varSubject2Prefix = "p",
+                varSubject2Full = "Physics",
+                varSubject3Prefix = "m",
+                varSubject3Full = "Maths",
+                varHobby1 = "Sports",
+                varHobby2 = "Music",
+                varPicture = "Screenshot 2020-11-20 at 02.47.53.png",
                 varCurrentAddress = "Tester Strasse 123, Berlin",
-                state = "",
-                city = "";
+                varState = "Haryana",
+                varCity = "Panipat";
 
         open("https://demoqa.com/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
@@ -37,38 +47,47 @@ public class StudentRegistrationFormTest {
         $("#firstName").val(varFirstName);
         $("#lastName").val(varLastName);
         $("#userEmail").val(varUserEmail);
-        $("#genterWrapper").$(byText("Other")).click();
+        $("#genterWrapper").$(byText(varGender)).click();
         $("#userNumber").val(varUserNumber);
 
-//      "Set Date" method starts
+//      "Set Date"
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("February");
-        $(".react-datepicker__year-select").selectOption("1991");
-        $(".react-datepicker__day--020").click();
-//      "Set Date" method ends
+        $(".react-datepicker__month-select").selectOption(varBirthMonth);
+        $(".react-datepicker__year-select").selectOption(varBirthYear);
+        $(".react-datepicker__day--0" + varBirthDay).click();
 
-//      "Set subject" method starts
-        $("#subjectsInput").val("e");
-        $(".subjects-auto-complete__menu-list").$(byText("English")).click();
-        $("#subjectsInput").val("p");
-        $(".subjects-auto-complete__menu-list").$(byText("Physics")).click();
-        $("#subjectsInput").val("m");
-        $(".subjects-auto-complete__menu-list").$(byText("Maths")).click();
-//      "Set subject" method ends
+//      "Set Subject"
+        $("#subjectsInput").val(varSubject1Prefix);
+        $(".subjects-auto-complete__menu-list").$(byText(varSubject1Full)).click();
+        $("#subjectsInput").val(varSubject2Prefix);
+        $(".subjects-auto-complete__menu-list").$(byText(varSubject2Full)).click();
+        $("#subjectsInput").val(varSubject3Prefix);
+        $(".subjects-auto-complete__menu-list").$(byText(varSubject3Full)).click();
 
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#hobbiesWrapper").$(byText("Music")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/Screenshot 2020-11-20 at 02.47.53.png"));
+        $("#hobbiesWrapper").$(byText(varHobby1)).click();
+        $("#hobbiesWrapper").$(byText(varHobby2)).click();
+
+        $("#uploadPicture").uploadFile(new File("src/test/resources/" + varPicture));
         $("#currentAddress").val(varCurrentAddress);
 
-        $("#state").click();
-//      $("#stateCity-wrapper").$(byText("Haryana")).click();
-        $("#react-select-3-option-2").click();
-        $("#city").click();
-//      $("#stateCity-wrapper").$(byText("Lucknow")).click();
-        $("#react-select-4-option-1").click();
+        $x("//div[@id='state']").click();
+        $("#stateCity-wrapper").$(byText(varState)).click();
+        $x("//div[@id='city']").click();
+        $("#stateCity-wrapper").$(byText(varCity)).click();
 
         $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $x("//td[text()='Student Name']").parent().shouldHave(text(varFirstName + " " + varLastName));
+        $x("//td[text()='Student Email']").parent().shouldHave(text(varUserEmail));
+        $x("//td[text()='Gender']").parent().shouldHave(text(varGender));
+        $x("//td[text()='Mobile']").parent().shouldHave(text(varUserNumber));
+        $x("//td[text()='Date of Birth']").parent().shouldHave(text(varBirthDay + " " + varBirthMonth + "," + varBirthYear));
+        $x("//td[text()='Subjects']").parent().shouldHave(text(varSubject1Full + "," + " " + varSubject2Full + "," + " " + varSubject3Full));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text(varHobby1 + "," + " " + varHobby2));
+        $x("//td[text()='Picture']").parent().shouldHave(text(varPicture));
+        $x("//td[text()='Address']").parent().shouldHave(text(varCurrentAddress));
+        $x("//td[text()='State and City']").parent().shouldHave(text(varState + " " + varCity));
 
     }
 }
